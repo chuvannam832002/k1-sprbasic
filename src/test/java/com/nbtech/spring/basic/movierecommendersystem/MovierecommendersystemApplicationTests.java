545fa17550c633d18c43945863e5bc940d9013e8
Chu Van Nam <chuvannam70@gmail.com>
2022-12-06 06:24:27 +0700

    Generate

---
+++ src/test/java/com/nbtech/spring/basic/movierecommendersystem/MovierecommendersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbtech.spring.basic.movierecommendersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierecommendersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
