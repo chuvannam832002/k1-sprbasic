ac196fffa98386962124759cf6237ac70ac19b02
Chu Van Nam <chuvannam70@gmail.com>
2022-12-06 07:24:55 +0700

    Movie recommender with contentbased filter

---
+++ src/main/java/com/nbtech/spring/basic/movierecommendersystem/Lession1/ContentBasedFilter.java
@@ -0,0 +1,7 @@
+package com.nbtech.spring.basic.movierecommendersystem.Lession1;
+
+public class ContentBasedFilter {
+    public String[] getRecommendations(String Movie){
+        return new String[]{"Happy Feet","Ice Ace","Up"};
+    }
+}
--- src/main/java/com/nbtech/spring/basic/movierecommendersystem/MovierecommendersystemApplication.java
+++ src/main/java/com/nbtech/spring/basic/movierecommendersystem/MovierecommendersystemApplication.java
@@ -1,13 +1,26 @@
 package com.nbtech.spring.basic.movierecommendersystem;
 
+import com.nbtech.spring.basic.movierecommendersystem.Lession1.MovieRecommender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MovierecommendersystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovierecommendersystemApplication.class, args);
+		//SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+
+		//create object of RecommenderImplementation class
+		MovieRecommender recommender = new MovieRecommender();
+
+		//call method to get recommendations
+		String[] result = recommender.recommendMovies("Finding Dory");
+
+		//display results
+		System.out.println(Arrays.toString(result));
 	}
 
 }
