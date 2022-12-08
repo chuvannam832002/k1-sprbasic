65f19119094d83b9f8b26c69c7fbb546fca9af9d
Chu Van Nam <chuvannam70@gmail.com>
2022-12-08 07:19:42 +0700

    Practice with primary and qualifier

--- src/main/java/com/nbtech/spring/basic/movierecommendersystem/Lession3/CollaborativeFilter.java
+++ src/main/java/com/nbtech/spring/basic/movierecommendersystem/Lession3/CollaborativeFilter.java
@@ -1,8 +1,10 @@
 package com.nbtech.spring.basic.movierecommendersystem.Lession3;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("CDF")
 public class CollaborativeFilter implements MovieFilter {
     @Override
     public String[] getRecommendations(String movie) {
--- src/main/java/com/nbtech/spring/basic/movierecommendersystem/Lession3/ContentBasedFilter.java
+++ src/main/java/com/nbtech/spring/basic/movierecommendersystem/Lession3/ContentBasedFilter.java
@@ -1,5 +1,9 @@
 package com.nbtech.spring.basic.movierecommendersystem.Lession3;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+@Primary
+@Component
 public class ContentBasedFilter implements MovieFilter {
     public String[] getRecommendations(String Movie){
         return new String[]{"Happy Feet","Ice Ace","Up"};
